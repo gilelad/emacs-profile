12dc965734b5c1c66d050ae9c0fa66db34dcab5b
Gil Elad <gilelad1@gmail.com>
2015-05-11 23:20:50 +0300

    fix elpy

--- init/init-elpy.el
+++ init/init-elpy.el
@@ -1,6 +1,4 @@
 (provide 'init-elpy)
 
 (elpy-enable)
-(elpy-use-ipython)
-(setq-default elpy-interactive-python-command "ipython3"
-              elpy-rpc-python-command         "ipython3")
+(elpy-use-ipython "ipython3")
