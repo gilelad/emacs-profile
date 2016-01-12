37c796c12369af8b1384521616580980f1f43b5b
gile <gile@checkpoint.com>
2016-01-12 07:59:33 +0200

    fix evil indentation

--- init/init-evil.el
+++ init/init-evil.el
@@ -1,4 +1,3 @@
-(provide 'init-evil)
 (require 'evil)
 
 (setq evil-emacs-state-cursor '("red" bar))
@@ -13,13 +12,26 @@
 
 (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
 
+;; use emacs-mode instead of insert-mode, for extended navigation shortcuts
 (add-hook 'evil-insert-state-entry-hook (lambda ()
                                           (evil-emacs-state)))
 
+;; Indent intelligently using "="
+(defun ge/evil-indent-correctly ()
+  ""
+  (interactive)
+  (if (use-region-p)
+      (indent-region (region-beginning) (region-end))
+    (indent-according-to-mode)))
+(define-key evil-normal-state-map (kbd "=") 'ge/evil-indent-correctly)
+
+;; activatition/deactivation shortcut
 (global-set-key (kbd "<f5>") 'evil-local-mode)
 
+;; don't move cursor back a step when exiting insert mode
 (setq evil-move-cursor-back nil)
 
+;; auto-activate evil on non-sepcial buffers
 (require 'utils)
 (defun auto-evil-local ()
   (if (and (not (string-match "^\\*" (buffer-name)))
@@ -27,3 +39,5 @@
       (evil-local-mode 1)
     nil))
 (add-hook 'create-buffer-mode-hook 'auto-evil-local)
+
+(provide 'init-evil)
