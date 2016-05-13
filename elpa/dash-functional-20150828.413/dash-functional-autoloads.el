7f215d143dba466d9236cf85168de48867ab50b8
Gil Elad <gilelad1@gmail.com>
2016-05-13 13:46:11 +0300

    add package dependencies. Remove actual packages

--- elpa/dash-functional-20150828.413/dash-functional-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-functional-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash-functional.el") (22063 44172 365840
-;;;;;;  895000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-functional-autoloads.el ends here
