586f8f3a4a80616c15a24a91559a267b4986319f
gilelad <gilelad1@gmail.com>
2016-05-22 08:37:33 +0300

    linux dev servers proxy

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
     ;;; init.el --- user init file      -*- no-byte-compile: t -*-
 
-(setenv "HTTP_PROXY" "http://proxy71.checkpoint.com:8080")
-(setenv "HTTPS_PROXY" "http://proxy71.checkpoint.com:8080")
+(setenv "HTTP_PROXY" "http://proxy51.checkpoint.com:8080")
+(setenv "HTTPS_PROXY" "http://prox51.checkpoint.com:8080")
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
