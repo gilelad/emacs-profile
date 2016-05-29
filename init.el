b128f89edeefbba9e25724a211e5b830b49fa48c
unknown <gile@gile-800.ad.checkpoint.com>
2016-05-29 09:33:39 +0300

    streamline init - handler-alist, gc-cons
    Conflicts:
    	init.el

--- init.el
+++ init.el
@@ -1,35 +1,40 @@
     ;;; init.el --- user init file      -*- no-byte-compile: t -*-
 
-(setenv "HTTP_PROXY" "http://proxy71.checkpoint.com:8080")
-(setenv "HTTPS_PROXY" "http://proxy71.checkpoint.com:8080")
+(let ((file-name-handler-alist nil)     ; avoid regexp matching during startup
+      (gc-cons-threshold 100000000))    ; avoid GC during startup
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file)
-;;--------------------------------------------------------
-;; Load 3rd party packages and initialization scripts
-;;--------------------------------------------------------
-(add-to-list 'load-path (concat user-emacs-directory "init")) ;; init scripts for 3rd party packages
-(add-to-list 'load-path (concat user-emacs-directory "lisp")) ;; my own lisp extensions
+  (setenv "HTTP_PROXY" "http://proxy71.checkpoint.com:8080")
+  (setenv "HTTPS_PROXY" "http://proxy71.checkpoint.com:8080")
 
-(defun ge/require (name)
-  "require wrapper with 'done' message"
-  (message "loading %s ... " name)
-  (require name))
+  (setq custom-file (concat user-emacs-directory "custom.el"))
+  (load custom-file)
+  ;;--------------------------------------------------------
+  ;; Load 3rd party packages and initialization scripts
+  ;;--------------------------------------------------------
+  (add-to-list 'load-path (concat user-emacs-directory "init")) ;; init scripts for 3rd party packages
+  (add-to-list 'load-path (concat user-emacs-directory "lisp")) ;; my own lisp extensions
 
-(ge/require 'init-lisp-loader)
+  (defun ge/require (name &optional opt)
+    "require wrapper with 'done' message"
+    (message "loading %s ... " name)
+    (require name nil opt))
 
-(ge/require 'ge-utils)
-(ge/require 'init-evil)
-(ge/require 'init-helm)
-(ge/require 'init-helm-projectile)
-(ge/require 'init-repls)
-(ge/require 'init-prog)
-(ge/require 'init-nav)
-(ge/require 'init-vis)
-(ge/require 'init-misc)
-(ge/require 'init-keys)
+  (ge/require 'init-lisp-loader)
 
-;; site-specific initializations - ignored by git
-(when (file-directory-p (concat user-emacs-directory "mylocal"))
-  (add-to-list 'load-path (concat user-emacs-directory "mylocal"))
-  (ge/require 'init-mylocal nil t))
+  (ge/require 'ge-utils)
+  (ge/require 'init-evil)
+  (ge/require 'init-helm)
+  (ge/require 'init-helm-projectile)
+  (ge/require 'init-repls)
+  (ge/require 'init-prog)
+  (ge/require 'init-nav)
+  (ge/require 'init-vis)
+  (ge/require 'init-misc)
+  (ge/require 'init-keys)
+
+  ;; site-specific initializations - ignored by git
+  (when (file-directory-p (concat user-emacs-directory "mylocal"))
+    (add-to-list 'load-path (concat user-emacs-directory "mylocal"))
+    (ge/require 'init-mylocal t))
+
+  )
