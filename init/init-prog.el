80c438e83a05b691637a82113a54a3d7c4b9c3ee
unknown <gile@gile-800.ad.checkpoint.com>
2016-07-10 10:42:30 +0300

    Revert "remove cscope dependency when not relevant"
    This reverts commit ff7fe6824c45a47ee40bab14c9e0df6c9ec717cf.

--- init/init-prog.el
+++ init/init-prog.el
@@ -106,4 +106,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; ECB ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; (require 'init-ecb)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;; CSCOPE ;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'xcscope)
+(add-hook 'c-mode-common-hook #'cscope-minor-mode)
+
 (provide 'init-prog)
