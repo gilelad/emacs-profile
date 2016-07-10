ff2d7932c14c266a999eb8430d80aaff57f25ce4
gilelad <gilelad1@gmail.com>
2016-07-10 10:10:05 +0300

    spaceline

--- init/init-vis.el
+++ init/init-vis.el
@@ -4,20 +4,20 @@
 (setq-default vertical-scroll-bar nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; MODE-LINE ;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'powerline)
-(require 'powerline-evil)
+;; (require 'powerline)
+;; (require 'powerline-evil)
 
-(require 'airline-themes)
-;; airline seperators
-(setq airline-utf-glyph-separator-left      #xe0b0
-      airline-utf-glyph-separator-right     #xe0b2
-      airline-utf-glyph-subseparator-left   #xe0b1
-      airline-utf-glyph-subseparator-right  #xe0b3
-      airline-utf-glyph-branch              #xe0a0
-      airline-utf-glyph-readonly            #xe0a2
-      airline-utf-glyph-linenumber          #xe0a1)
+;; (require 'airline-themes)
+;; ;; airline seperators
+;; (setq airline-utf-glyph-separator-left      #xe0b0
+;;       airline-utf-glyph-separator-right     #xe0b2
+;;       airline-utf-glyph-subseparator-left   #xe0b1
+;;       airline-utf-glyph-subseparator-right  #xe0b3
+;;       airline-utf-glyph-branch              #xe0a0
+;;       airline-utf-glyph-readonly            #xe0a2
+;;       airline-utf-glyph-linenumber          #xe0a1)
 
-(load-theme 'airline-kolor)
+;; (load-theme 'airline-kolor)
 ;; (unless (display-graphic-p)
 ;;   (require 'evil-terminal-cursor-changer))
 ;; Must have these after loading themes
@@ -27,5 +27,9 @@
       evil-insert-state-cursor evil-emacs-state-cursor
       evil-replace-state-cursor '("#cdcd00" hbar)
       evil-operator-state-cursor '("#cecd00" hollow))
+
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
+(setq spaceline-highlight-face-func #'spaceline-highlight-face-evil-state)
 
 (provide 'init-vis)
